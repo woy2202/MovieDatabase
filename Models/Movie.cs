d3885c0dd64990f96e2150c7d0b7477f9e85178c
woy22 <woy2202@gmail.com>
2025-01-25 12:28:52 +0100

    initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FilmowaBaza.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } // Id filmu
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; } // Tytuł filmu
+
+        [Required]
+        public int ReleaseYear { get; set; } // Rok produkcji
+
+        [Required]
+        [MaxLength(100)]
+        public string Director { get; set; } // Reżyser
+
+        [MaxLength(50)]
+        public string Genre { get; set; } // Gatunek
+        [MaxLength(50)]
+        public string Country_of_origin { get; set; } // Kraj produkcji 
+    }
+    
+    
+}
+
+
